d5425ca58fa65ab892fbc109e9bb615d3f68edd1
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-21 10:55:35 +0300

    004

--- gamemodel.hpp
+++ gamemodel.hpp
@@ -22,7 +22,14 @@ public:
 
     IView* keyEventsHandler(IView* sender, const int key) final
     {
-
+        switch (key)
+        {
+          case 'w': if(m_cursor.destinationCell.y > 0) m_cursor.destinationCell -= Vec2d(0,1); break;
+          case 'a': if(m_cursor.destinationCell.x > 0) m_cursor.destinationCell -= Vec2d(1,0); break;
+          case 's': if(m_cursor.destinationCell.y < Size.y) m_cursor.destinationCell += Vec2d(0,1); break;
+          case 'd': if(m_cursor.destinationCell.x < Size.x) m_cursor.destinationCell += Vec2d(1,0); break;
+          case ' ': break;
+        }
     }
 
     IView* mouseEventsHandler(IView* sender, const MouseRecord& mr) final;
