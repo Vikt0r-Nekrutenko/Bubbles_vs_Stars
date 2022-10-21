78d087ec02ea8be02d9da0ce9473f178554a0255
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-21 10:52:36 +0300

    003

--- gamemodel.hpp
+++ gamemodel.hpp
@@ -9,18 +9,27 @@ using namespace stf::smv;
 
 class GameModel : public BaseModel
 {
+    struct Cursor
+    {
+        Vec2d selectedCell {0,0};
+        Vec2d destinationCell {0,0};
+    };
+
 public:
     const Vec2d Size { 8, 8 };
 
     GameModel();
 
-    IView* keyEventsHandler(IView* sender, const int key) final;
+    IView* keyEventsHandler(IView* sender, const int key) final
+    {
+
+    }
 
     IView* mouseEventsHandler(IView* sender, const MouseRecord& mr) final;
 
 private:
 
-    Vec2d m_cursor;
+    Cursor m_cursor;
     uint8_t m_player = 'o';
 };
 
