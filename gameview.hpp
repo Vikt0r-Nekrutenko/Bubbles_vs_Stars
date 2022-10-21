5a44d35be0d8c4f1945527edbf39c79fdd497831
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-21 11:10:47 +0300

    007

--- gameview.hpp
+++ gameview.hpp
@@ -3,6 +3,7 @@
 
 #include "iview.hpp"
 #include "sprite.hpp"
+#include "gamemodel.hpp"
 
 using namespace stf;
 using namespace stf::smv;
@@ -14,6 +15,16 @@ public:
     virtual void show(Renderer &renderer)
     {
         m_board.show(renderer, true);
+        Vec2d pzero = renderer.Size / 2 - m_board.Size() / 2;
+
+        GameModel *gameModel = static_cast<GameModel*>(m_model);
+        Vec2d cursorPos = gameModel->cursor().destinationCell;
+        auto cell = [&](const Vec2d pos) -> const Vec2d {
+          return pzero + m_board.markers().at(gameModel->Size.x * pos.y + pos.x + 1);
+        };
+
+        renderer.drawFrame(cell(cursorPos) - Vec2d(1,0), Vec2d(3,1));
+//        renderer.drawPixel(cell({-1,0}), static_cast<GameModel*>(m_model)->player());
     }
 
 private:
--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include "window.hpp"
+#include "gameview.hpp"
 
 using namespace std;
 using namespace stf;
 
 class Game : public Window
 {
+    GameModel gameModel = GameModel();
+    GameView gameView = GameView(&gameModel);
 public:
 
     bool onUpdate(const float dt) final
     {
-        s.show(renderer, true);
+        gameView.show(renderer);
         return true;
     }
 
     void keyEvents(const int key) final
     {
-
+        gameView.keyEventsHandler(key);
     }
 
     void mouseEvents(const MouseRecord &mr) final
     {
-
+        gameView.mouseEventsHandler(mr);
     }
 };
 
