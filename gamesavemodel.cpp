92a03b4bf7dcdcbae6815685ffdc956004b05ab1
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-28 13:34:39 +0300

    052

--- gamesavemodel.cpp
+++ gamesavemodel.cpp
@@ -1,4 +1,14 @@
 #include "gamesavemodel.hpp"
+#include "gamemodel.hpp"
 
 GameSaveModel::GameSaveModel(GameModel *model)
     : StackModel("bvss.sdb"), m_model(model) {}
+
+void GameSaveModel::load()
+{
+    for(int i = 0; i < 64; ++i)
+        m_board[i] = m_model->board()[i];
+    m_plOneScore = m_model->plOneScore();
+    m_plTwoScore = m_model->plTwoScore();
+    m_plTwoScore = m_model->player();
+}
