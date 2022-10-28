d5540f53ec50b59d0af5079e3d82204a81d1aaef
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-28 18:27:02 +0300

    063

--- menuview.cpp
+++ menuview.cpp
@@ -1,7 +1,7 @@
 #include "menuview.hpp"
 #include "gameview.hpp"
 #include "gamemodel.hpp"
-//#include "storyview.hpp"
+#include "storyview.hpp"
 #include "closeview.hpp"
 #include "pausemenuview.hpp"
 
@@ -46,7 +46,7 @@ IView* MenuView::menuSelectConfirm()
         static_cast<GameModel*>(m_model)->saves.load();
         return new PauseMenuView(static_cast<GameModel*>(m_model));
       } catch(...) { return this; }
-//    case 2: return new StoryView(static_cast<GameModel*>(m_model), this);
+    case 2: return new StoryView(static_cast<GameModel*>(m_model), this);
     case 3: return new CloseView(static_cast<GameModel*>(m_model));
   }
   return this;
