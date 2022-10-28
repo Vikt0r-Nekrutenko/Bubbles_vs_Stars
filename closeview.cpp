f8eb3cf0902800343a42d3b02dcf69fd7e71c3ab
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-28 13:13:50 +0300

    050

--- closeview.cpp
+++ closeview.cpp
@@ -2,10 +2,7 @@
 #include "gamemodel.hpp"
 
 CloseView::CloseView(GameModel* model)
-  : IView(model)
-  {
-//    static_cast<GameModel*>(m_model)->saveTree();
-  }
+  : IView(model) {}
 
 bool CloseView::isContinue() const { return false; }
 
